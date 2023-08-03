2cb257821a3c2164df058ddf72143ddffcee0c94
Valmo Trindade <70849974+valmojr@users.noreply.github.com>
2023-08-03 18:18:57 +0000

    added custom color palette

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { DiscordLogoIcon } from '@radix-ui/react-icons';
 export default function Page() {
 	return (
 		<main className="">
-			<Link href="/Main"><button className="flex flex-row flex-nowrap bg-red-400">
+			<Link href="/Main"><button className="flex flex-row flex-nowrap bg-[blurple]">
 				<DiscordLogoIcon/>
 				Main
 				</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        blurple: "5865F2",
+        Green: "57F287",
+        Yellow: "FEE75C",
+        Fuchsia: "EB459E",
+        Red: "ED4245",
+        White: "FFFFFF",
+        Black: "000000",
+      },
     },
   },
   plugins: [],
