30c570c39dfdf42a9b045e344ae68be44e375716
valmojr <val_mo_jr@hotmail.com>
2023-08-07 19:53:00 -0300

    upgraded button components

--- src/app/Components/Buttons/DiscordButton.tsx
+++ src/app/Components/Buttons/DiscordButton.tsx
@@ -2,18 +2,18 @@ import { ButtonProps } from './PrimaryButton';
 import { twMerge } from 'tailwind-merge';
 
 export default function DiscordButton({
-	success,
-	className,
-	...props
+  success,
+  className,
+  ...props
 }: ButtonProps) {
-	return (
-		<button
-			data-success={success}
-			className={twMerge(
-				'flex flex-row flex-nowrap rounded-xl bg-[#5865F2] w-48 h-14 items-center justify-center data-[success=true]:bg-emerald-500',
-				className
-			)}
-			{...props}
-		/>
-	);
+  return (
+    <button
+      data-success={success}
+      className={twMerge(
+        'flex flex-row flex-nowrap rounded-xl bg-Blurple w-48 h-14 items-center justify-center data-[success=true]:bg-Green data-[success=false]:bg-Red',
+        className,
+      )}
+      {...props}
+    />
+  );
 }
--- src/app/Components/Buttons/PrimaryButton.tsx
+++ src/app/Components/Buttons/PrimaryButton.tsx
@@ -2,13 +2,13 @@ import { ComponentProps } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export type ButtonProps = ComponentProps<'button'> & {
-	success?: boolean;
+  success?: boolean | undefined;
 };
 
 export default function PrimaryButton({
-	success,
-	className,
-	...props
+  success,
+  className,
+  ...props
 }: ButtonProps) {
-	return <button className={twMerge('', className)} {...props} />;
+  return <button className={twMerge('', className)} {...props} />;
 }
