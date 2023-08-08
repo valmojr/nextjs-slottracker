2a89b8862c80df44e8b6709b8cebc470e1f9e278
valmojr <val_mo_jr@hotmail.com>
2023-08-08 11:48:29 -0300

    refactored soonest event frame on dashboard, added framer motion and correct sizing (mobile first only)

--- src/app/Main/SoonestEvents/EventFrame.tsx
+++ src/app/Main/SoonestEvents/EventFrame.tsx
@@ -1,10 +1,11 @@
 import { Event } from '@/app/Util/types';
+import { motion } from 'framer-motion';
 
 export default function EventFrame({ event }: { event: Event }) {
-	return (
-		<div className="w-full h-16 bg-gray-500">
-			<h1>{event.title}</h1>
-			<h3>{event.description}</h3>
-		</div>
-	);
+  return (
+    <motion.div className="w-full h-16 bg-gray-500">
+      <h1>{event.title}</h1>
+      <h3>{event.description}</h3>
+    </motion.div>
+  );
 }
--- src/app/Main/SoonestEvents/SoonestEventFrame.tsx
+++ src/app/Main/SoonestEvents/SoonestEventFrame.tsx
@@ -1,16 +1,18 @@
-import { randomEventsExample } from '@/app/Util/loremipsum';
+'use client';
 import { Event } from '@/app/Util/types';
 import EventFrame from './EventFrame';
+import { motion } from 'framer-motion';
 
-export default function SoonestEventFrame() {
-	const assignedEventInOrderOfDate: Event[] = randomEventsExample;
-
-	return (
-		<div className="aspect-video bg-red-500">
-			<h1 className="">Coming Soon</h1>
-			{assignedEventInOrderOfDate.map((event, index) => (
-				<EventFrame key={index} event={event} />
-			))}
-		</div>
-	);
+export default function SoonestEventFrame({
+  assignedEvents,
+}: {
+  assignedEvents: Event[];
+}) {
+  return (
+    <motion.div className="aspect-video rounded-xl bg-DarkDarken">
+      {assignedEvents.map((event, index) => (
+        <EventFrame key={index} event={event} />
+      ))}
+    </motion.div>
+  );
 }
