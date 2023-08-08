621c5f4e3e2853be1d02b8f05175fb2cc9adfa16
valmojr <val_mo_jr@hotmail.com>
2023-08-08 11:48:56 -0300

    refactored Dashboard page to accept the framer motion child components

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -1,14 +1,21 @@
+import { loggedUserExample } from '../Util/loremipsum';
+import { Event } from '../Util/types';
 import AvaliableEventsFrame from './AvaliableEvents/AvaliableEventsFrame';
+import BottomButton from './Bottom Menu/BottomButton';
 import BottonMenuFrame from './Bottom Menu/BottomMenuFrame';
 import SoonestEventFrame from './SoonestEvents/SoonestEventFrame';
 
 export default function MainPage() {
-	return (
-		<div>
-			<h1 className='font-bold'>Dashboard</h1>
-			<SoonestEventFrame />
-			<AvaliableEventsFrame />
-			<BottonMenuFrame />
-		</div>
-	);
+  const userInfo = loggedUserExample;
+  const userAssignedEvents: Event[] = [];
+  const userCreatedEvents: Event[] = [];
+  const avaliableEvents: Event[] = [];
+
+  return (
+    <div className="flex flex-col flex-nowrap gap-4 w-screen h-screen justify-start px-2 py-2 bg-DarkDarkest">
+      <SoonestEventFrame assignedEvents={userAssignedEvents} />
+      <AvaliableEventsFrame />
+      <BottonMenuFrame user={userInfo} />
+    </div>
+  );
 }
