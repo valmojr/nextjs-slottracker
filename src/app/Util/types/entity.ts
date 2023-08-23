854bdec73919a33496b219047599ff8da35ee3b2
valmojr <val_mo_jr@hotmail.com>
2023-08-22 23:51:41 -0300

    added props and entity types files

---
+++ src/app/Util/types/entity.ts
@@ -0,0 +1,38 @@
+export type Event = {
+  id: string;
+  title: string;
+  description: string;
+  thumbnail?: string;
+  starts_at: string;
+  ends_at: string;
+  created_at: string;
+  updated_at: string;
+  ownerId: string;
+};
+
+export type Group = {
+  id: string;
+  name: string;
+  eventId: string;
+};
+
+export type Role = {
+  id: string;
+  name: string;
+  groupId: string;
+  userId: string;
+};
+
+export type User = {
+  id: string;
+  username: string;
+  avatar: string;
+  accessToken?: string;
+  refreshToken?: string;
+};
+
+export type Session = {
+  id: string;
+  expiredAt: string;
+  json: string;
+};
---
+++ src/app/Util/types/props.ts
@@ -0,0 +1,3 @@
+import { ComponentProps } from 'react';
+
+export type ButtonProps = { success?: boolean } & ComponentProps<'button'>;
