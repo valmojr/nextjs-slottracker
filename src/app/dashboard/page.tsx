6295cd0290cd2d860627d95f6e0678a9ee73be4b
valmojr <val_mo_jr@hotmail.com>
2023-08-25 23:22:54 -0300

    dashboard page consuming UserProfile context, now is time to have some fun with tailwindcss

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,13 @@
+'use client';
+import { useContext } from 'react';
+import { ProfileContext, ProfileContextProps } from '../context/Context';
+
 export default function DashboardPage() {
-  return <h1>Dashboard Page</h1>;
+  const { profile } = useContext(ProfileContext) as ProfileContextProps;
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      <h2>{profile?.username}</h2>
+    </>
+  );
 }
