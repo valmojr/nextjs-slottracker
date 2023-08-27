30089027ca05f9ff94755cecf1cfe006e36d0766
valmojr <val_mo_jr@hotmail.com>
2023-08-27 18:20:14 -0300

    added default colors as object exported

---
+++ src/app/Util/JSONs/colors.ts
@@ -0,0 +1,23 @@
+export const DarkLightest = '#424549';
+export const DarkLighten = '#36393e';
+export const DarkDarken = '#282b30';
+export const DarkDarkest = '#1e2124';
+export const Blurple = '#5865F2';
+export const Green = '#57F287';
+export const Yellow = '#FEE75C';
+export const Fuchsia = '#EB459E';
+export const Red = '#ED4245';
+
+const colors = {
+  DarkLightest,
+  DarkLighten,
+  DarkDarken,
+  DarkDarkest,
+  Blurple,
+  Green,
+  Yellow,
+  Fuchsia,
+  Red,
+};
+
+export default colors;
