51f7a97ecf6adbf73349310782d8f26a05f8af24
valmojr <val_mo_jr@hotmail.com>
2023-11-07 19:22:49 -0300

    extentionaed create event component

--- src/app/dashboard/create/createEvent.tsx
+++ src/app/dashboard/create/createEvent.tsx
@@ -35,35 +35,34 @@ export default function CreateEvent({ avaliableGuilds }: CreateEventComponentPro
     if (!avaliableGuilds) {
         return <h1>{'Loading User Guilds'}</h1>
     } else {
-
         return <>
             <Dialog.Content style={{ maxWidth: 450 }}>
                 <Dialog.Title>Create Event</Dialog.Title>
                 <Flex direction="column" gap="3">
                     <label>
                         <Text as="div" size="2" mb="1" weight="bold">
-                            teste
+                            Title
                         </Text>
-                        <TextField.Input placeholder="Event Title" />
+                        <TextField.Input placeholder="Event Title" onChange={(e: any)=>handleEventTitle(e)} />
                     </label>
                     <label>
                         <Text as="div" size="2" mb="1" weight="bold">
                             Description
                         </Text>
-                        <TextField.Input placeholder="Event Description" />
+                        <TextField.Input placeholder="Event Description" onChange={(e: any)=>handleEventDescription(e)}/>
                     </label>
                     <label>
                         <Text as="div" size="2" mb="1" weight="bold">
                             Thumbnail
                         </Text>
-                        <TextField.Input placeholder="Thumbnail URL" />
+                        <TextField.Input placeholder="Thumbnail URL"  onChange={(e: any)=>handleEventThumbnail(e)}/>
                     </label>
                     <label>
                     <RadioGroup.Root className='flex flex-col gap-1' defaultValue="default" aria-label="View density">
                         <Text as="div" size="2" mb="1" weight="bold">
                             Group/Server
                         </Text>
-                    {avaliableGuilds.map((guild) => (
+                            {avaliableGuilds.map((guild) => (
                             <div key={guild.id} className='flex flex-row items-center'>
                                 <RadioGroup.Item className='bg-white w-3 h-3 rounded-full shadow-md hover:bg-[var-(--violet-3)] focus:shadow-md' value="default" id="r1">
                                     <RadioGroup.Indicator className={`flex items-center justify-center w-full h-full relative after:w-1 after:h-1 after:content-[''] after:rounded-xl after:block after:bg-[var(--violet-11)]`} />
@@ -76,11 +75,6 @@ export default function CreateEvent({ avaliableGuilds }: CreateEventComponentPro
                     ))}
                     </RadioGroup.Root>
                     </label>
-                    <h1>Check</h1>
-                        <h2>Title: {eventTitle}</h2>
-                        <h2>Description: {eventDescription}</h2>
-                        <h2>Thumbnail: {eventThumbnail}</h2>
-                        <h2>Guild: {eventGuild?.name}</h2>
                 </Flex>
                 <Flex gap="3" mt="4" justify="end">
                     <Dialog.Close>
@@ -89,7 +83,11 @@ export default function CreateEvent({ avaliableGuilds }: CreateEventComponentPro
                         </Button>
                     </Dialog.Close>
                     <Dialog.Close>
-                        <Button color="green" type={'submit'}>Create</Button>
+                        <Button
+                            color="green"
+                            type={'submit'}
+                            onClick={() => null}
+                        >Create</Button>
                     </Dialog.Close>
                 </Flex>
             </Dialog.Content>
